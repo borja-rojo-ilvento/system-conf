71400bb63874a760e7b48048be742b1fa448aa0c
Borja Rojo Ilvento <borja.rojo@gmail.com>
2025-06-17 20:48:33 -0400

    Remove the technic-launcher shit

--- hosts/nixos/techinic-launcher.nix
+++
@@ -1,8 +0,0 @@
-{ stdenv, fetchurl }: stdenv.mkDerivation {
-
-  name = "Technic Launcher";
-  src = {
-    url = "https://launcher.technicpack.net/launcher4/900/TechnicLauncher.jar";
-    hash = "";
-  };
-}
